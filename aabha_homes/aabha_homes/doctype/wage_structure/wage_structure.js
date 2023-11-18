585014b74a2e2a01af666abccf7a04ffb76e6293
sammish <sammish.thundiyil@gmail.com>
2023-11-18 03:00:32 +0000

    update

--- aabha_homes/aabha_homes/doctype/wage_structure/wage_structure.js
+++ aabha_homes/aabha_homes/doctype/wage_structure/wage_structure.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2023, sammish and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("State", {
+// frappe.ui.form.on("Wage Structure", {
 // 	refresh(frm) {
 
 // 	},
