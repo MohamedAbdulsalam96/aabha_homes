7f09f3aed2b264bfb39d4c2553e37d67d7f62387
bibinvabaraham <bibincv237@gmail.com>
2023-12-16 20:22:36 +0530

    bugfix:swe-cancel

--- aabha_homes/aabha_homes/doctype/wage_structure/wage_structure.js
+++ aabha_homes/aabha_homes/doctype/wage_structure/wage_structure.js
@@ -1,8 +1,14 @@
 // Copyright (c) 2023, sammish and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Wage Structure", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Wage Structure", {
+	onload(frm) {
+        frm.set_query('worker_name', function(frm){
+            return{
+                filters:{
+                    custom_is_worker:1
+                }
+            }
+        })
+	},
+});
