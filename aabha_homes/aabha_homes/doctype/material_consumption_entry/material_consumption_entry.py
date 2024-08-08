de47b0f258c362af2f81fb20f2b5957accd09f5f
BibinVinoy <bibincv237@gmail.com>
2024-08-08 12:13:57 +0530

    edit posting time-mce

--- aabha_homes/aabha_homes/doctype/material_consumption_entry/material_consumption_entry.py
+++ aabha_homes/aabha_homes/doctype/material_consumption_entry/material_consumption_entry.py
@@ -12,12 +12,17 @@ class MaterialConsumptionEntry(Document):
 		se_doc.stock_entry_type = "Material Issue"
 		se_doc.from_warehouse = self.project_warehouse
 		se_doc.custom_material_consumption_entry = self.name
+		se_doc.set_posting_time = True
+		se_doc.posting_date = self.posting_date
+		se_doc.posting_time = self.time
+
 		mcd = frappe.db.get_all("Material Consumption Details",{"parent": self.name},
 		["item_name as item_code", "item_qty as qty", "uom", "uom as stock_uom"])
 		for item in mcd:
 			item["cost_center"] = self.cost_center
 			item["s_warehouse"] = self.project_warehouse
 			item["conversion_factor"] = 1
+			# item["allow_zero_valuation_rate"] = 1
 			se_doc.append("items", item)
 		se_doc.save(ignore_permissions=True)
 		se_doc.submit()
